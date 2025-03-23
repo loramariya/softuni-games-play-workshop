6430996a9e305258364990890d9a6912f4ec3061
loramariya <loramaria.raynova@gmail.com>
2025-03-23 18:06:38 +0200

    Delete game functionality

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 import gameService from "../../services/gameService";
 export default function GameDetails() {
+    const navigate = useNavigate();
     const [game, setGame] = useState({});
     const { gameId } = useParams();
 
@@ -12,55 +13,72 @@ export default function GameDetails() {
         })();
     }, [gameId]);
 
-  return (
-    <section id="game-details">
-    <h1>Game Details</h1>
-    <div className="info-section">
+    const gameDeleteClickHandler = async () => {
+        const hasConfirm = confirm(`Are you sure you want to delete ${game.title} game?`);
 
-        <div className="game-header">
-            <img className="game-img" src={game.imageUrl} />
-            <h1>{game.title}</h1>
-            <span className="levels">MaxLevel: {game.maxLevel}</span>
-            <p className="type">{game.category}</p>
-        </div>
+        if (!hasConfirm) {
+            return;
+        }
+        
+       await gameService.delete(gameId);
 
-        <p className="text">
-           {game.summary}
-        </p>
+       navigate('/games');
 
-        {/* <!-- Bonus ( for Guests and Users ) --> */}
-        <div className="details-comments">
-            <h2>Comments:</h2>
-            <ul>
-                {/* <!-- list all comments for current game (If any) --> */}
-                <li className="comment">
-                    <p>Content: I rate this one quite highly.</p>
-                </li>
-                <li className="comment">
-                    <p>Content: The best game.</p>
-                </li>
-            </ul>
-            {/* <!-- Display paragraph: If there are no games in the database --> */}
-            <p className="no-comment">No comments.</p>
-        </div>
+    }
+    return (
+        <section id="game-details">
+            <h1>Game Details</h1>
+            <div className="info-section">
 
-        {/* <!-- Edit/Delete buttons ( Only for creator of this game )  --> */}
-        <div className="buttons">
-            <a href="#" className="button">Edit</a>
-            <a href="#" className="button">Delete</a>
-        </div>
-    </div>
+                <div className="game-header">
+                    <img className="game-img" src={game.imageUrl} />
+                    <h1>{game.title}</h1>
+                    <span className="levels">MaxLevel: {game.maxLevel}</span>
+                    <p className="type">{game.category}</p>
+                </div>
 
-    {/* <!-- Bonus -->
+                <p className="text">
+                    {game.summary}
+                </p>
+
+                {/* <!-- Bonus ( for Guests and Users ) --> */}
+                <div className="details-comments">
+                    <h2>Comments:</h2>
+                    <ul>
+                        {/* <!-- list all comments for current game (If any) --> */}
+                        <li className="comment">
+                            <p>Content: I rate this one quite highly.</p>
+                        </li>
+                        <li className="comment">
+                            <p>Content: The best game.</p>
+                        </li>
+                    </ul>
+                    {/* <!-- Display paragraph: If there are no games in the database --> */}
+                    <p className="no-comment">No comments.</p>
+                </div>
+
+                {/* <!-- Edit/Delete buttons ( Only for creator of this game )  --> */}
+                <div className="buttons">
+                    <Link to="#" className="button">Edit</Link>
+                    <button
+                        onClick={gameDeleteClickHandler}
+                        className="button"
+                    >
+                        Delete
+                    </button>
+                </div>
+            </div>
+
+            {/* <!-- Bonus -->
     <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) --> */}
-    <article className="create-comment">
-        <label>Add new comment:</label>
-        <form className="form">
-            <textarea name="comment" placeholder="Comment......"></textarea>
-            <input className="btn submit" type="submit" value="Add Comment" />
-        </form>
-    </article>
+            <article className="create-comment">
+                <label>Add new comment:</label>
+                <form className="form">
+                    <textarea name="comment" placeholder="Comment......"></textarea>
+                    <input className="btn submit" type="submit" value="Add Comment" />
+                </form>
+            </article>
 
-</section>
-  );
+        </section>
+    );
 }
