790c34317994365921328b1ab48d6ac16b8cf344
loramariya <loramaria.raynova@gmail.com>
2025-03-23 19:16:33 +0200

    Add login handler

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import {Routes, Route} from 'react-router'
+import { Routes, Route } from 'react-router'
 
 import Header from './components/header/Header';
 import Home from './components/home/Home';
@@ -9,24 +9,30 @@ import GameCreate from './components/game-create/GameCreate';
 import GameDetails from './components/game-details/GameDetails';
 import GameEdit from './components/game-edit/GameEdit';
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  const userLoginHandler = (email) => {
+    setEmail(email);
+  }
 
   return (
     <div id="box">
-        <Header/>
+      <Header />
 
-        <main id="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/games" element={<GameCatalog />} />
-            <Route path="/games/create" element={<GameCreate />} />
-            <Route path="/games/:gameId/details" element={<GameDetails />} />
-            <Route path="/games/:gameId/edit" element={<GameEdit />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </main>
+      <main id="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/games" element={<GameCatalog />} />
+          <Route path="/games/create" element={<GameCreate />} />
+          <Route path="/games/:gameId/details" element={<GameDetails />} />
+          <Route path="/games/:gameId/edit" element={<GameEdit />} />
+          <Route path="/login" element={<Login  onLogin={userLoginHandler}/>} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </main>
     </div>
   )
 }
--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,24 +1,35 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
-export default function Login() {
+export default function Login({
+  onLogin,
+}) {
+  const navigate = useNavigate();
+  const loginAction = (formData) => {
+    const email = formData.get('email');
+
+    onLogin(email);
+
+    navigate('/games');
+  }
+
   return (
     <section id="login-page" className="auth">
-    <form id="login">
+      <form id="login" action={loginAction}>
 
         <div className="container">
-            <div className="brand-logo"></div>
-            <h1>Login</h1>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+          <div className="brand-logo"></div>
+          <h1>Login</h1>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
 
-            <label htmlFor="login-pass">Password:</label>
-            <input type="password" id="login-password" name="password" />
-            <input type="submit" className="btn submit" value="Login" />
-            <p className="field">
-                <span>If you don't have profile click <Link to="/register">here</Link></span>
-            </p>
+          <label htmlFor="login-pass">Password:</label>
+          <input type="password" id="login-password" name="password" />
+          <input type="submit" className="btn submit" value="Login" />
+          <p className="field">
+            <span>If you don't have profile click <Link to="/register">here</Link></span>
+          </p>
         </div>
-    </form>
-</section>
+      </form>
+    </section>
   );
 }
